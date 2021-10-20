cd1644626d4b821a4fdd0d8dac64a0c808c8258e
yizhishuai <42272807+yizhishuai@users.noreply.github.com>
2021-10-20 12:42:39 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,10 +1,10 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  title: 'YIZHISHUAI',
+  author: 'yizhishuai',
+  email: 'liushuaidejia@qq.com',
+  link: 'https://yizhishuai.xyz',
+  description: 'Personal life, study and work records.',
+  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
